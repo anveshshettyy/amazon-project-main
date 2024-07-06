f1de65a73cedf450020e58fa3d1f0502c5d49fbc
anveshshettyy <thomigang03@gmail.com>
2024-07-06 18:41:59 +0530

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,23 +1,38 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts , loadProductsFetch} from "../data/products.js";
-import { loadCart  } from "../data/cart.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practise.js'
 
-Promise.all([
-   loadProductsFetch(),
-    new Promise((resolve) => {
+async function loadPage() {
+
+    await loadProductsFetch();
+
+    await new Promise((resolve) => {
         loadCart(() => {
             resolve();
         });
-    })
+    });
 
-]).then((values) => {
-    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
-}); 
+}
+loadPage();
+
+// Promise.all([
+//    loadProductsFetch(),
+//     new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//     })
+
+// ]).then((values) => {
+//     console.log(values);
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
 
 // new Promise((resolve) => {
 //     loadProducts(() => {
