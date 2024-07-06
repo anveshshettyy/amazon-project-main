7cddc862c631a8b1f16ce04e3d97da15376939a3
anveshshettyy <thomigang03@gmail.com>
2024-07-06 16:18:02 +0530

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from "../scripts/utils/money.js";
+import { formatCurrency } from "../scripts/utils/money.js";
 
 export function getProduct(productId) {
   let matchingProduct;
@@ -19,20 +19,20 @@ class Product {
   priceCents;
 
   constructor(productDetails) {
-    this.id =productDetails.id;
+    this.id = productDetails.id;
     this.image = productDetails.image;
     this.name = productDetails.name;
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
   }
 
-  getStarsUrl(){
+  getStarsUrl() {
     return `images/ratings/rating-${this.rating.stars * 10}.png`;
   }
-  getPrice(){
+  getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
-  extraInfoHTML(){
+  extraInfoHTML() {
     return '';
   }
 }
@@ -53,14 +53,14 @@ const product1 = new Product({
   ]
 });
 
-class Clothing extends Product{
+class Clothing extends Product {
   sizeChartLink;
 
-  constructor(productDetails){
+  constructor(productDetails) {
     super(productDetails);
     this.sizeChartLink = productDetails.sizeChartLink;
   }
-  extraInfoHTML(){
+  extraInfoHTML() {
     return `
       <a href="${this.sizeChartLink}" target="_blank">
       Size Chart
@@ -95,7 +95,30 @@ class Clothing extends Product{
 // }
 // object3();
 
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type == 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/*  
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -761,3 +784,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
 // import '../data/cart-class.js';
-import '../data/backend-practise.js'
-
-
-renderOrderSummary();
-renderPaymentSummary();
+// import '../data/backend-practise.js'
